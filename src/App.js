51edde3c6535a4f038f8275dd33be12d8dc0b5f5
Teona <teonazaalishvili2@gmail.com>
2023-04-04 12:02:05 -0400

    implemented filter

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import fakeFetch from './utils/fakeFetch'
 import Filter from './components/Filter'
 import Header from './components/Header';
 import Person from './components/Person';
+import "./App.css";
 
 function App() {
 
   const [ people, setPeople ] = useState([])
-  const [devLevelFilter, setDevLevelFilter] = useState("")
+  const [filter, setFilter] = useState("")
+
+  async function getPeople() {
+    const JSON_Response = await fakeFetch(); //don't move to the next line,until this line is fully done running.  Gets Data from a fetch call
+    const Javascript_Response = await JSON_Response.json(); //.json converts JSON object to JavaScript
+    setPeople(await Javascript_Response); // set the people variable to equal the Javascript_Response
+  }
 
   useEffect(()=> {
-    const getPeople = async () => {
-      const res = await fakeFetch() // get data from "fetch"
-      setPeople(await res.json()) // store the result of .json() in state
-    }
-    getPeople()
+    
+    getPeople();
   },[])
 
   return (
     <div className="app">
       <Header />
+      <Filter setFilter={setFilter}/>
       <div className="people-div d-flex flex-wrap justify-content-center">
-          { people.map(person => <Person key={person.id} person={person} />) }
+          { people.filter(person => !filter || person.devLevel === filter)
+          .map((person) => (
+            <Person key={person.id} person={person} />
+          ))}
+         
       </div>
     </div>
   );
